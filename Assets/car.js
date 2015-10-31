9e8a0f32a080af118b11fb64ffef6b3cc740022f
komjaur <komjaur@gmail.com>
2015-10-31 04:38:29 +0200

    a
    a

--- Assets/car.js
+++ Assets/car.js
@@ -1,116 +1,42 @@
-﻿#pragma strict
-var redlineRPM:float = 6150;
-var maxRPM:float = 7000;
+﻿//CarController1.js
 
-var finalDrive:float;
-var gearRatios:float[];
-
-//public bool hasAutoTransmission = true;
-     
-
-var damagePerc:float; //??
-     
-
-private var currentRPM:float;
-private var currentDamage:float;
-private var currentSpeed:float;
-var currentGear:int;
-private var reverse:boolean;
-private var maxDamage:float = 100;
-
-function Awake()
-{
-    //tireCircumference = tireDimensions.Circumference / 12;
-}
-  
-function Update()
-{
-    var changeUp:boolean = Input.GetKeyDown(KeyCode.Q);
-    var changeDown:boolean = Input.GetKeyDown(KeyCode.A);
-    if (changeUp || changeDown)
-    {
-        ChangeGear(changeUp);
-    }
-         
-    reverse = Input.GetKey(KeyCode.DownArrow);
-    if(Input.GetKey(KeyCode.UpArrow))
-    {
-        if(currentRPM < maxRPM)
-        {
-            currentRPM += 10;
-        }
-    }
-    else
-    {
-        currentRPM -= reverse ? 15 : 5;
-    }    
-    if (currentRPM < 0) 
-    {
-        currentRPM = 0;
-    }
-         
  
-    currentSpeed = ((currentRPM * 0.2) / (gearRatios[currentGear] * finalDrive * 88));// * 0.44704f;    // 1 mph = 0.44704 m/s
-  
-    Debug.Log(currentSpeed);
-         
-    // move the object
-    transform.Translate(Vector3.forward * ( Time.deltaTime) / 2);
+var enginePower=150.0;
+
+var maxSpeed=50;
+var power=0.0;
+var brake:float=0;
+var steer:float=0;
+var maxSteer=25.0;
+private var currentSpeed:float=0;
+private var rb:Rigidbody;
+function Start(){
+	rb=GetComponent.<Rigidbody>();
+    rb.centerOfMass=Vector3(0,-0.5,0.3);
 }
-     
-function ChangeGear(changeUp:boolean)
-{
-    Debug.Log(changeUp);     
-    if ((changeUp && currentGear < gearRatios.Length-1) || (!changeUp && currentGear > 0))
-    {
-        currentGear += (changeUp ? 1 : -1);
-        //currentRPM *= (changeUp ? 0.5f : 2);
-    }
-}
-     
-
-/*var maxSpeed:float;
-var maxDamage:int;
-var maxGear:int;
-var maxRPM:int;
-var manualGearing:boolean;
-var currSpeed:float;
-private var currDamage:int;
-var currRPM:int;
-private var redRPM:int;
-private var gear:int;
-
-
-var rb: Rigidbody;
-function Start () {
-    rb = GetComponent.<Rigidbody>();
-
-}
-function GearUp()
-{
-
-
-}
+ 
 function Update () {
-    
-    
-    //currentSpeed = ((currentRPM * tireCircumference) / (gearRatios[currentGear] * finalDrive * 88))
-    currSpeed = (currRPM / 2 );
-    if (Input.GetKey ("space"))
-    {
-        
-        if(currRPM < maxRPM)
-        {
-            currRPM += 10;
-        }
-        rb.AddForce(transform.forward * Time.deltaTime*currSpeed);
-        //transform.Translate(Vector3.forward * Time.deltaTime*currSpeed);
-    }else
-    {
-        currRPM -=10;
-    }
-    if (currRPM < 0) 
-    {
-        currRPM = 0;  
-    }
-}*/
+	power=Input.GetAxis("Vertical") * enginePower * Time.deltaTime * 50.0;
+	steer=Input.GetAxis("Horizontal") * maxSteer;
+	brake=Input.GetKey("space") ? GetComponent.<Rigidbody>().mass * 0.1: 0.0;
+	if (power!=0)
+	{	
+		currentSpeed = rb.velocity.magnitude;
+
+		if (currentSpeed>maxSpeed)
+		{
+			rb.velocity = rb.velocity.normalized *maxSpeed ;
+		}else
+		{
+			
+			rb.AddRelativeForce (Vector3.forward * power);
+			
+			
+		}
+	}
+
+	if (steer!=0)
+	{
+		transform.Rotate(Vector3.up * Time.deltaTime *steer*5);
+	}
+}
