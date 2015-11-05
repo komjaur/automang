159346fd921397ca8685238e0c96b54803b75ed7
komjaur <komjaur@gmail.com>
2015-11-05 07:34:56 +0200

    Update 13.8:lähen kooli kohe
    -interface süsteem tegin

--- Assets/skript/gameplay2.js
+++ Assets/skript/gameplay2.js
@@ -7,13 +7,55 @@ var interfaceHolders:GameObject[];
 
 var carspawn:GameObject[];
 var walkersspawn:GameObject[];
-
+var Maps:GameObject[];
 function Start () {
 
 }
 
 function Update () {
 
+}
+function selectmap(mapID:int)
+{
+	var currentscene=GameObject.Find("currentgamescene").transform;
+	//Debug.Log(currentscene.childCount);
+	for (var i = currentscene.transform.childCount - 1; i >= 0; i--)
+	{
+		Destroy(currentscene.transform.GetChild(i).gameObject);
+	}
+ 
+	
+	
+	
+	
+	var currentmap = Instantiate(Maps[mapID],Vector3(0,0,0), Quaternion.Euler(Vector3(-90, 0, 0)));
+	currentmap.transform.parent = currentscene;
+	
+	interfacefunctions(1);
+	/*
+	switch (mapid)
+    {
+	    case 1:
+	        
+	        break;
+	    case 2:
+	        print ("Hello and good day!");
+	        break;
+	    case 3:
+	        print ("Whadya want?");
+	        break;
+	    case 4:
+	        print ("Grog SMASH!");
+	        break;
+	    case 5:
+	        print ("Ulg, glib, Pblblblblb");
+	        break;
+	    default:
+	        print ("ei ole sellist mapi");
+	        break;
+    }
+    */
+    
 }
 function interfacefunctions(whereto:int)
 {
