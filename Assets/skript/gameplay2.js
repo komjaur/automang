6495c5a35ee72a558964686a8cb69aeb21d06eb2
komjaur <komjaur@gmail.com>
2015-11-06 10:37:30 +0200

    Update 13.9:Nädalavahetus tuleb
    -koodi tegin

--- Assets/skript/gameplay2.js
+++ Assets/skript/gameplay2.js
@@ -5,9 +5,11 @@
 var interfaceHolders:GameObject[];
 
 
-var carspawn:GameObject[];
-var walkersspawn:GameObject[];
+
 var Maps:GameObject[];
+
+var CarSpawn:Transform[];
+ var WalkerSpawn:Transform[];
 function Start () {
 
 }
@@ -17,46 +19,53 @@ function Update () {
 }
 function selectmap(mapID:int)
 {
-	var currentscene=GameObject.Find("currentgamescene").transform;
-	//Debug.Log(currentscene.childCount);
-	for (var i = currentscene.transform.childCount - 1; i >= 0; i--)
-	{
-		Destroy(currentscene.transform.GetChild(i).gameObject);
-	}
- 
-	
-	
-	
-	
-	var currentmap = Instantiate(Maps[mapID],Vector3(0,0,0), Quaternion.Euler(Vector3(-90, 0, 0)));
-	currentmap.transform.parent = currentscene;
-	
-	interfacefunctions(1);
-	/*
-	switch (mapid)
+    var currentscene=GameObject.Find("currentgamescene").transform;
+    //Debug.Log(currentscene.childCount);
+    for (var i = currentscene.transform.childCount - 1; i >= 0; i--)
     {
-	    case 1:
-	        
-	        break;
-	    case 2:
-	        print ("Hello and good day!");
-	        break;
-	    case 3:
-	        print ("Whadya want?");
-	        break;
-	    case 4:
-	        print ("Grog SMASH!");
-	        break;
-	    case 5:
-	        print ("Ulg, glib, Pblblblblb");
-	        break;
-	    default:
-	        print ("ei ole sellist mapi");
-	        break;
+        Destroy(currentscene.transform.GetChild(i).gameObject);
     }
-    */
+    var currentmap = Instantiate(Maps[mapID],Vector3(0,0,0), Quaternion.Euler(Vector3(-90, 0, 0)));
+    currentmap.transform.parent = currentscene;
+	
+    interfacefunctions(1);
+    CarSpawn = new CountAndFill("spawnpoint","carspawn");
+    WalkerSpawn = new CountAndFill("spawnpoint","walkerspawn");
+
     
 }
+
+function CountAndFill(tagName : String ,objectName : String)
+{
+    var objects = GameObject.FindGameObjectsWithTag(tagName);
+    var array :Transform[];
+    for (var x=0;x<3;x++)
+    {
+        var counter = 0;
+        for(var object in objects)
+        {
+            if (object.name == objectName)
+            {
+
+                if (x == 1)
+                {
+                    array[counter] = object.gameObject.transform;
+                }
+                counter++;
+            }
+    
+        }
+        if (x == 0)
+        {
+            array = new Transform[counter];
+        }
+    }
+
+    return array;
+
+}
+
+
 function interfacefunctions(whereto:int)
 {
 	
